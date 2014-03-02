ae17f9c03459da9a884f3ba677203b87c094ad73
Joslyn <jrosbrook@gmail.com>
2014-03-02 10:02:07 -0800

    configure API

--- test/anagram_test.rb
+++ test/anagram_test.rb
@@ -1,12 +1,10 @@
-require "test_helper"
+load "test_helper.rb"
+require "anagram"
 
 class AnagramTest < Minitest::Unit::TestCase
   def test_solve
     anagram = Anagram.new("hamlet", "amleth")
     assert anagram.solve 
-
-    
-
   end 
 
 end
